875506ebdab51c01584fecaf98269145f40164be
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-09-27 10:31:26 +0200

    feat(nvim): add `lua/user/options.lua`
    Take content from the `:options` command and comment every line:
    
        Open a window for viewing and setting all options. Options are
        grouped by function. Offers short help for each option.  Hit <CR> on
        the short help to open a help window with more help for the option.
        Modify the value of the option and hit <CR> on the "set" line to set
        the new value.  For window and buffer specific options, the last
        accessed window is used to set the option value in, unless this is a
        help window, in which case the window below help window is used
        (skipping the option-window).

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,3 +1,5 @@
 -- File containing initialization commands is generically called a "vimrc" or
 -- config file.  It can be either Vimscript ("init.vim") or Lua ("init.lua"),
 -- but not both.
+
+require("user.options")
