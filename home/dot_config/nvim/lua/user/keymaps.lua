676dde37b0c4eba7fd5219a982e00f260f19d746
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-09-28 23:15:51 +0200

    feat(nvim): place cursor at center of window after various operations

--- home/dot_config/nvim/lua/user/keymaps.lua
+++ home/dot_config/nvim/lua/user/keymaps.lua
@@ -1,3 +1,5 @@
+-- Heavily inspired by: https://github.com/ThePrimeagen/.dotfiles
+
 -- Modes when using an arrow keys to navigate is not beneficial.
 local arrow_key_modes = {"i", "n", "o", "t", "v"}
 
@@ -11,3 +13,9 @@ vim.keymap.set(arrow_key_modes, "<down>", arrow_key)
 vim.keymap.set(arrow_key_modes, "<left>", arrow_key)
 vim.keymap.set(arrow_key_modes, "<right>", arrow_key)
 vim.keymap.set(arrow_key_modes, "<up>", arrow_key)
+
+-- Place the cursor at the center of the window after various operations.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv")
