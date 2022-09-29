73632abfb265a72aca3d18fea00c04d306873761
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-09-29 23:37:40 +0200

    docs(nvim): remove obvious comments

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -2,14 +2,7 @@
 -- config file. It can be either Vimscript ("init.vim") or Lua ("init.lua"), but
 -- not both.
 
--- Set autocommands.
 require("user.autocommands")
-
--- Set keymaps.
 require("user.keymaps")
-
--- Set options.
 require("user.options")
-
--- Set plugins.
 require("user.plugins")
