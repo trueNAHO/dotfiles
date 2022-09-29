821b036921fb3b6143cda5e75f412c1d2cafaf97
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-09-29 23:38:53 +0200

    feat(nvim): change loading order in `init.lua`

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -2,7 +2,7 @@
 -- config file. It can be either Vimscript ("init.vim") or Lua ("init.lua"), but
 -- not both.
 
-require("user.autocommands")
-require("user.keymaps")
 require("user.options")
+require("user.keymaps")
+require("user.autocommands")
 require("user.plugins")
