cc24cfdaaf1e2fb8a1a4c4d70b1dce5b7a333331
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-09-29 23:27:35 +0200

    feat(nvim): replace `onedark` color scheme by `tokyonight-storm`

--- home/dot_config/nvim/lua/user/plugins.lua
+++ home/dot_config/nvim/lua/user/plugins.lua
@@ -25,11 +25,11 @@ return require("packer").startup(function()
 
     -- Color scheme.
     --require("user.plugins.catppuccin_nvim")
-    require("user.plugins.joshdick_onedark_vim")
+    --require("user.plugins.joshdick_onedark_vim")
     --require("user.plugins.tokyonight_nvim_day")
     --require("user.plugins.tokyonight_nvim_moon")
     --require("user.plugins.tokyonight_nvim_night")
-    --require("user.plugins.tokyonight_nvim_storm")
+    require("user.plugins.tokyonight_nvim_storm")
 
     -- Automatically set up your configuration after cloning `packer.nvim`. Put
     -- this at the end after all plugins
