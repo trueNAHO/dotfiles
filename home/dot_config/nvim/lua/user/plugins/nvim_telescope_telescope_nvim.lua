6a10ee2f807f9f134a855864123eabd88488dc8e
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-04 23:32:23 +0200

    feat(nvim): add file picker keymaps for `telescope`

--- home/dot_config/nvim/lua/user/plugins/nvim_telescope_telescope_nvim.lua
+++ home/dot_config/nvim/lua/user/plugins/nvim_telescope_telescope_nvim.lua
@@ -5,5 +5,27 @@ require("packer").use{
     requires = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-fzf-native.nvim"
-    }
+    },
+    config = function()
+        local telescope_builtin = require("telescope.builtin")
+
+        local leader_prefix = "<leader>f"
+
+        -- Search for files (respecting .gitignore).
+        vim.keymap.set("n", leader_prefix .. "ff", telescope_builtin.find_files)
+
+        -- Fuzzy search for files tracked by Git. This command lists the output
+        -- of the `git ls-files` command, respects .gitignore, and optionally
+        -- ignores untracked files.
+        vim.keymap.set("n", leader_prefix .. "fg", telescope_builtin.git_files)
+
+        -- Searches for the string under your cursor in your current working
+        -- directory.
+        vim.keymap.set("n", leader_prefix .. "fl",
+                       telescope_builtin.grep_string)
+
+        -- Search for a string and get results live as you type (respecting
+        -- .gitignore).
+        vim.keymap.set("n", leader_prefix .. "fs", telescope_builtin.live_grep)
+    end
 }
