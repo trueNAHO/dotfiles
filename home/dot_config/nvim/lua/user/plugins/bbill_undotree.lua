ea3362e6295646778bb47191d6a3655f45a0fc58
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-06 23:12:23 +0200

    feat(nvim): customise diff panel for `undotree` plugin

--- home/dot_config/nvim/lua/user/plugins/bbill_undotree.lua
+++ home/dot_config/nvim/lua/user/plugins/bbill_undotree.lua
@@ -4,6 +4,8 @@ require("packer").use{
     config = function()
         local leader_prefix = "<leader>u"
 
+        vim.g.undotree_CustomDiffpanelCmd = "botright 10 new"
+
         -- Toggle the undo-tree panel.
         vim.keymap.set("n", leader_prefix,
                        function() vim.cmd("UndotreeToggle") end)
