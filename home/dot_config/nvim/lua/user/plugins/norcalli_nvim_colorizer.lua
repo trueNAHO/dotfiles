3f93f211252b6d20e8d69d39a0ca135ac4628ca0
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-06 17:53:20 +0200

    feat(nvim): setup `nvim-colorizer` plugin

--- home/dot_config/nvim/lua/user/plugins/norcalli_nvim_colorizer.lua
+++ home/dot_config/nvim/lua/user/plugins/norcalli_nvim_colorizer.lua
@@ -1,2 +1,8 @@
 -- The fastest Neovim colorizer.
-require("packer").use("norcalli/nvim-colorizer.lua")
+require("packer").use{
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+        vim.opt.termguicolors = true
+        require("colorizer").setup()
+    end
+}
