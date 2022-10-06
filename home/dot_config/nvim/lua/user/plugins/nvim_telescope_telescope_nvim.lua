6091fd8d62a1361c22116e64f5abdcd41450578e
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-06 09:46:15 +0200

    feat(nvim): add `vim_pickers` for `telescope`

--- home/dot_config/nvim/lua/user/plugins/nvim_telescope_telescope_nvim.lua
+++ home/dot_config/nvim/lua/user/plugins/nvim_telescope_telescope_nvim.lua
@@ -22,8 +22,37 @@ local function telescope_config()
         vim.keymap.set("n", leader_prefix .. "lg", telescope_builtin.live_grep)
     end
 
+    local function vim_pickers()
+        -- Lists available plugin/user commands and runs them on `<cr>`.
+        vim.keymap.set("n", leader_prefix .. "ca", telescope_builtin.commands)
+
+        -- Lists commands that were executed recently, and reruns them on
+        -- `<cr>`.
+        vim.keymap.set("n", leader_prefix .. "ch",
+                       telescope_builtin.command_history)
+
+        -- Lists available help tags and opens a new window with the relevant
+        -- help info on `<cr>`.
+        vim.keymap.set("n", leader_prefix .. "ht", telescope_builtin.help_tags)
+
+        -- Lists items in the quickfix list, jumps to location on `<cr>`.
+        vim.keymap.set("n", leader_prefix .. "qf", telescope_builtin.quickfix)
+
+        -- Lists vim options, allows you to edit the current value on `<cr>`.
+        vim.keymap.set("n", leader_prefix .. "vo",
+            telescope_builtin.vim_options)
+
+        -- Lists normal mode keymappings, runs the selected keymap on `<cr>`.
+        vim.keymap.set("n", leader_prefix .. "km", telescope_builtin.keymaps)
+
+        -- Live fuzzy search inside of the currently open buffer.
+        vim.keymap.set("n", leader_prefix .. "fb",
+                       telescope_builtin.current_buffer_fuzzy_find)
+    end
+
     local function main()
         file_pickers()
+        vim_pickers()
     end
 
     main()
