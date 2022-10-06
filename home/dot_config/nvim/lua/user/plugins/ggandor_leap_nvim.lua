e0ba0f1bbd7938146543aad295b68010e27a632e
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-06 22:56:30 +0200

    feat(nvim): setup `leap` plugin

--- home/dot_config/nvim/lua/user/plugins/ggandor_leap_nvim.lua
+++ home/dot_config/nvim/lua/user/plugins/ggandor_leap_nvim.lua
@@ -1,3 +1,31 @@
 -- Neovim's answer to the mouse: a "clairvoyant" interface that makes on-screen
 -- jumps quicker and more natural than ever.
-require("packer").use("ggandor/leap.nvim")
+require("packer").use{
+    "ggandor/leap.nvim",
+    config = function()
+        local leap = require("leap")
+
+        local leader_prefix = "<leader>f"
+
+        leap.opts = {
+            -- Disable auto-jumping to the first match.
+            safe_labels = {},
+        }
+
+        -- Bidirectional search. Initiate multi-window mode with the current
+        -- window as the only target.
+        vim.keymap.set({"n", "v"}, leader_prefix .. "f", function()
+            leap.leap{target_windows = {vim.fn.win_getid()}}
+        end)
+
+        -- Search in all windows.
+        vim.keymap.set({"n", "v"}, leader_prefix .. "w", function()
+            leap.leap{target_windows = vim.tbl_filter(
+                function(win)
+                    return vim.api.nvim_win_get_config(win).focusable
+                end,
+                vim.api.nvim_tabpage_list_wins(0)
+            )}
+        end)
+    end
+}
