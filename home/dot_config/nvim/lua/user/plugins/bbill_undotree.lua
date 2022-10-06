daec491d4000dc8d2ca1cfc0e19e13b5103d58af
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-06 17:24:17 +0200

    feat(nvim): add keymap to toggle undo-tree panel

--- home/dot_config/nvim/lua/user/plugins/bbill_undotree.lua
+++ home/dot_config/nvim/lua/user/plugins/bbill_undotree.lua
@@ -1,2 +1,11 @@
 -- The undo history visualizer for VIM.
-require("packer").use("mbbill/undotree")
+require("packer").use{
+    "mbbill/undotree",
+    config = function()
+        local leader_prefix = "<leader>u"
+
+        -- Toggle the undo-tree panel.
+        vim.keymap.set("n", leader_prefix,
+                       function() vim.cmd("UndotreeToggle") end)
+    end
+}
