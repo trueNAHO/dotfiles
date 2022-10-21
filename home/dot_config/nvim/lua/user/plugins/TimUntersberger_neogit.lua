6886929eac528db07e31b37acef15965fafbefa1
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-21 16:42:17 +0200

    feat(nvim): add keymap to open `neogit`

--- home/dot_config/nvim/lua/user/plugins/TimUntersberger_neogit.lua
+++ home/dot_config/nvim/lua/user/plugins/TimUntersberger_neogit.lua
@@ -4,10 +4,21 @@ require("packer").use({
     requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
     config = function()
         local neogit = require("neogit")
+
+        local leader = "<leader>g"
+
+        local prefix_desc = "Git. "
+
         neogit.setup({
             disable_commit_confirmation = true,
             integrations = { diffview = true },
             use_magit_keybindings = true
         })
+
+        -- Open a new NeogitStatus instance.
+        vim.keymap.set(
+            "n", leader, neogit.open,
+            { desc = prefix_desc .. "Open a new NeogitStatus instance." }
+        )
     end
 })
