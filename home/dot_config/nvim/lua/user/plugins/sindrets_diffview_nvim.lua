e2302d419202f29e16c011949fa3416217782b08
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-10-21 17:59:32 +0200

    feat(nvim): disable icons for `diffview` plugin

--- home/dot_config/nvim/lua/user/plugins/sindrets_diffview_nvim.lua
+++ home/dot_config/nvim/lua/user/plugins/sindrets_diffview_nvim.lua
@@ -2,5 +2,10 @@
 -- files for any git rev.
 require("packer").use({
     "sindrets/diffview.nvim",
-    requires = "nvim-lua/plenary.nvim"
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+        require("diffview").setup({
+            use_icons = false
+        })
+    end
 })
