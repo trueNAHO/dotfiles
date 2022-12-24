17c8ff6c508686f5b886b05dcad008824df79f9f
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-12-24 15:33:53 +0100

    feat(nvim): improve `sumneko_lua` LSP
    Specifying the workspace slows down startup time and causes an annoying
    prompt. Not specifying the workspace fixes those problems.

--- home/dot_config/nvim/lua/user/plugins/VonHeikemen_lsp-zero_nvim.lua
+++ home/dot_config/nvim/lua/user/plugins/VonHeikemen_lsp-zero_nvim.lua
@@ -77,10 +77,6 @@ require("packer").use({
                             -- Get the language server to recognize the `vim`
                             -- global.
                             globals = { "vim" }
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files.
-                            library = vim.api.nvim_get_runtime_file("", true)
                         }
                     }
                 }
