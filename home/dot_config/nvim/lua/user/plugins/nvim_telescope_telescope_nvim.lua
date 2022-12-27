2a84f1b2f5141cd4fa0e3e7cb40b887be0bdcf37
NAHO <90870942+trueNAHO@users.noreply.github.com>
2022-12-27 15:34:54 +0100

    feat(nvim): `telescope` no longer uses fixed tag

--- home/dot_config/nvim/lua/user/plugins/nvim_telescope_telescope_nvim.lua
+++ home/dot_config/nvim/lua/user/plugins/nvim_telescope_telescope_nvim.lua
@@ -181,7 +181,6 @@ end
 -- Find, Filter, Preview, Pick. All lua, all the time.
 require("packer").use({
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.0",
     requires = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-fzf-native.nvim",
