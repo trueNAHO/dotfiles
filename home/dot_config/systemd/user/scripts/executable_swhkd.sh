8ffce34a28e98166b2c14b23a7a656be17b216fc
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-06-18 15:40:58 +0200

    feat(systemd): add `swkd.servicd`

---
+++ home/dot_config/systemd/user/scripts/executable_swhkd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+killall swhks || :
+
+# "Sxhkd clone for Wayland (works on TTY and X11 too)". "Because swhkd can be
+# used anywhere, the same swhkd config can be used across Xorg or Wayland
+# desktops, and you can even use swhkd in a TTY."
+# (https://github.com/waycrate/swhkd)
+swhks &
+pkexec swhkd --config "${XDG_CONFIG_DIR:-$HOME/.config}/swhkd/swhkdrc"
