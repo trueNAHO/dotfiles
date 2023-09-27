29877c0e26f7c206c5427288b811b944b9849b6c
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-09-27 18:23:28 +0200

    refactor(homemanager/stylix): add initial version

---
+++ homeManager/stylix/default.nix
@@ -0,0 +1,32 @@
+{pkgs, ...}: {
+  stylix = {
+    fonts = {
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      monospace = {
+        package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+        name = "FiraCodeNerdFont";
+      };
+
+      sansSerif = {
+        package = pkgs.ibm-plex;
+        name = "IBMPlexSans";
+      };
+
+      serif = {
+        package = pkgs.crimson;
+        name = "Crimson";
+      };
+    };
+
+    image = pkgs.fetchurl {
+      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    };
+
+    polarity = "dark";
+  };
+}
