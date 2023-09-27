21428e7df9e63a97db88e9ee1631a824566e5a1f
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-09-27 18:19:10 +0200

    refactor(homemanager/home/packages): add initial version

---
+++ homeManager/home/packages/default.nix
@@ -0,0 +1,16 @@
+{
+  imports = [
+    ./du-dust
+    ./dua
+    ./duf
+    ./entr
+    ./fd
+    ./glow
+    ./gping
+    ./killall
+    ./pipe-rename
+    ./procs
+    ./tldr
+    ./tokei
+  ];
+}
