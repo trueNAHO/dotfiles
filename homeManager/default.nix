ed0934bd9ba88e0daf25bc9f671a4718ac541984
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-09-27 18:23:49 +0200

    refactor(homemanager): add initial version

---
+++ homeManager/default.nix
@@ -0,0 +1,18 @@
+{config, ...}: {
+  imports = [
+    ./fonts
+    ./home
+    ./programs
+    ./services
+    ./stylix
+  ];
+
+  home = {
+    homeDirectory = "/home/${config.home.username}";
+    stateVersion = "23.05";
+    username = "naho";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true;
+}
