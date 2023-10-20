ef6813707c317775cb262a4423ca8677decefb15
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-10-20 22:28:06 +0200

    feat(modules/homeManager/programs/rofi): customize theme
    Requires: https://github.com/danth/stylix/issues/176

--- modules/homeManager/programs/rofi/default.nix
+++ modules/homeManager/programs/rofi/default.nix
@@ -13,6 +13,26 @@
     cfg = config.modules.homeManager.programs.rofi;
   in
     lib.mkIf (cfg.enable || cfg.pass.enable) {
+      home.file."${config.programs.rofi.configPath}".text = let
+        largePadding = toString 10;
+        smallPadding = toString 5;
+      in ''
+        element {
+          padding: ${smallPadding}px;
+          spacing: ${smallPadding}px;
+        }
+
+        inputbar {
+          children: [ prompt, entry ];
+          padding: ${largePadding}px ${largePadding}px;
+          spacing: ${largePadding}px;
+        }
+
+        window {
+          width: 25%;
+        }
+      '';
+
       programs.rofi = {
         enable = cfg.enable;
         package = pkgs.rofi-wayland;
