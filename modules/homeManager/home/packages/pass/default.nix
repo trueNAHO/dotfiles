186549dfdf8447fd69283658d18aaabb881178c1
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-04 18:15:13 +0100

    feat(modules/homeManager/home/packages/pass): use Wayland package

--- modules/homeManager/home/packages/pass/default.nix
+++ modules/homeManager/home/packages/pass/default.nix
@@ -8,6 +8,6 @@
     lib.mkEnableOption "pass";
 
   config = lib.mkIf config.modules.homeManager.home.packages.pass.enable {
-    home.packages = [pkgs.pass];
+    home.packages = [pkgs.pass-wayland];
   };
 }
