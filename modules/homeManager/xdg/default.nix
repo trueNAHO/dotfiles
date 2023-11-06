73349fca2052344ee063c58afd553cfae2784d49
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-06 13:14:43 +0100

    feat(modules/homeManager/xdg): configure XDG user directories

--- modules/homeManager/xdg/default.nix
+++ modules/homeManager/xdg/default.nix
@@ -6,6 +6,19 @@
   options.modules.homeManager.xdg.enable = lib.mkEnableOption "xdg";
 
   config = lib.mkIf config.modules.homeManager.xdg.enable {
-    xdg.enable = true;
+    xdg = {
+      enable = true;
+
+      userDirs = {
+        desktop = config.home.sessionVariables.TMPDIR;
+        documents = config.home.sessionVariables.TMPDIR;
+        download = config.home.sessionVariables.TMPDIR;
+        music = "${config.home.homeDirectory}/p/music";
+        pictures = "${config.home.homeDirectory}/p/images";
+        publicShare = config.home.sessionVariables.TMPDIR;
+        templates = config.home.sessionVariables.TMPDIR;
+        videos = "${config.home.homeDirectory}/p/videos";
+      };
+    };
   };
 }
