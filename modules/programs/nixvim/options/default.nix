cd916b63ad084234fa232004ce9b4aac7e533b56
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-07 14:00:35 +0100

    refactor(modules/programs/nixvim/options): properly append to options
    Addresses: https://github.com/nix-community/nixvim/issues/680

--- modules/programs/nixvim/options/default.nix
+++ modules/programs/nixvim/options/default.nix
@@ -1,22 +1,27 @@
 {
-  programs.nixvim.options = {
-    breakindent = true;
-    colorcolumn = "+1";
-    cursorline = true;
-    expandtab = true;
-    foldmethod = "marker";
-    ignorecase = true;
-    matchpairs = "(:),{:},[:]" + ",<:>";
-    mouse = "a";
-    relativenumber = true;
-    scrolloff = 8;
-    shiftwidth = 4;
-    shortmess = "filnxtToOF" + "I";
-    smartcase = true;
-    smartindent = true;
-    softtabstop = 4;
-    tabstop = 4;
-    textwidth = 80;
-    undofile = true;
+  programs.nixvim = {
+    extraConfigLua = ''
+      vim.opt.matchpairs:append("<:>")
+      vim.opt.shortmess:append("I")
+    '';
+
+    options = {
+      breakindent = true;
+      colorcolumn = "+1";
+      cursorline = true;
+      expandtab = true;
+      foldmethod = "marker";
+      ignorecase = true;
+      mouse = "a";
+      relativenumber = true;
+      scrolloff = 8;
+      shiftwidth = 4;
+      smartcase = true;
+      smartindent = true;
+      softtabstop = 4;
+      tabstop = 4;
+      textwidth = 80;
+      undofile = true;
+    };
   };
 }
