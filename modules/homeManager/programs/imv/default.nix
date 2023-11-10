f7fb192f5536bee956930c6d22da5e600855ec89
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-10 17:16:07 +0100

    feat(modules/homeManager/programs/imv): add 'n' and 'p' aliases

--- modules/homeManager/programs/imv/default.nix
+++ modules/homeManager/programs/imv/default.nix
@@ -6,6 +6,13 @@
   options.modules.homeManager.programs.imv.enable = lib.mkEnableOption "imv";
 
   config = lib.mkIf config.modules.homeManager.programs.imv.enable {
-    programs.imv.enable = true;
+    programs.imv = {
+      settings.binds = {
+        n = "next";
+        p = "prev";
+      };
+
+      enable = true;
+    };
   };
 }
