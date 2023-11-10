d7d65d285b5a12296d46d12eee07c42514271c33
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-10 20:14:06 +0100

    feat(modules/homeManager/programs/mpv): theme UI plugin
    Requires: https://github.com/danth/stylix/issues/185

--- modules/homeManager/programs/mpv/default.nix
+++ modules/homeManager/programs/mpv/default.nix
@@ -4,16 +4,31 @@
   pkgs,
   ...
 }: {
+  imports = [../../../stylix];
   options.modules.homeManager.programs.mpv.enable = lib.mkEnableOption "mpv";
 
   config = lib.mkIf config.modules.homeManager.programs.mpv.enable {
+    modules.stylix.enable = true;
+
     programs.mpv = {
       enable = true;
 
       scriptOpts = {
         playlistmanager.key_showplaylist = "F8";
         thumbfast.network = "yes";
-        uosc.top_bar_controls = "no";
+
+        uosc = let
+          colors = config.lib.stylix.colors;
+        in {
+          background = colors.base00;
+          background_text = colors.base05;
+          curtain = colors.base0D;
+          error = colors.base0F;
+          foreground = colors.base05;
+          foreground_text = colors.base00;
+          success = colors.base0A;
+          top_bar_controls = "no";
+        };
       };
 
       scripts = with pkgs.mpvScripts; [mpv-playlistmanager thumbfast uosc];
