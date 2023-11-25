08f0d0920a4a55cf6e8db9ecdb0476e020b37337
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-25 17:21:20 +0100

    feat(modules/homeManager/programs/alacritty): add settings

--- modules/homeManager/programs/alacritty/default.nix
+++ modules/homeManager/programs/alacritty/default.nix
@@ -7,6 +7,37 @@
     lib.mkEnableOption "alacritty";
 
   config = lib.mkIf config.modules.homeManager.programs.alacritty.enable {
-    programs.alacritty.enable = true;
+    programs.alacritty = {
+      enable = true;
+
+      settings = {
+        font.size = lib.mkForce 7;
+
+        key_bindings = [
+          {
+            key = "Key0";
+            mods = "Alt";
+            action = "ResetFontSize";
+          }
+
+          {
+            key = "Minus";
+            mods = "Alt";
+            action = "DecreaseFontSize";
+          }
+
+          {
+            key = "Equals";
+            mods = "Alt";
+            action = "IncreaseFontSize";
+          }
+        ];
+
+        window.padding = {
+          x = 2;
+          y = 2;
+        };
+      };
+    };
   };
 }
