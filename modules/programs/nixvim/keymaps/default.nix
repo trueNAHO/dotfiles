257e9fcbe1814fc74605911b17bb776e2eec398c
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-29 19:21:52 +0100

    fix(modules/programs/nixvim/keymaps): incorrect evaluation order

--- modules/programs/nixvim/keymaps/default.nix
+++ modules/programs/nixvim/keymaps/default.nix
@@ -1,82 +1,82 @@
 {
   programs.nixvim.keymaps =
     map (keymaps: {options.silent = true;} // keymaps)
-    (map (keymaps: {mode = "n";} // keymaps) [
-      {
-        action = "<C-d>zz";
-        key = "<C-d>";
-      }
+    ((map (keymaps: {mode = "n";} // keymaps) [
+        {
+          action = "<C-d>zz";
+          key = "<C-d>";
+        }
 
-      {
-        action = "<C-u>zz";
-        key = "<C-u>";
-      }
+        {
+          action = "<C-u>zz";
+          key = "<C-u>";
+        }
 
-      {
-        action = "<C-w>+";
-        key = "<M-C-j>";
-      }
+        {
+          action = "<C-w>+";
+          key = "<M-C-j>";
+        }
 
-      {
-        action = "<C-w>-";
-        key = "<M-C-k>";
-      }
+        {
+          action = "<C-w>-";
+          key = "<M-C-k>";
+        }
 
-      {
-        action = "<C-w><";
-        key = "<M-C-h>";
-      }
+        {
+          action = "<C-w><";
+          key = "<M-C-h>";
+        }
 
-      {
-        action = "<C-w>>";
-        key = "<M-C-l>";
-      }
+        {
+          action = "<C-w>>";
+          key = "<M-C-l>";
+        }
 
-      {
-        action = "Nzzzv";
-        key = "N";
-      }
+        {
+          action = "Nzzzv";
+          key = "N";
+        }
 
-      {
-        action = "function() local spell = not vim.opt.spell:get() vim.opt.spell = spell print(spell) end";
-        key = "<leader>s";
-        lua = true;
-      }
+        {
+          action = "function() local spell = not vim.opt.spell:get() vim.opt.spell = spell print(spell) end";
+          key = "<leader>s";
+          lua = true;
+        }
 
-      {
-        action = "nzzzv";
-        key = "n";
-      }
-    ])
-    ++ (map (keymaps: {mode = ["n" "v"];} // keymaps) [
-      {
-        action = ''"+Y'';
-        key = "<leader>Y";
-      }
+        {
+          action = "nzzzv";
+          key = "n";
+        }
+      ])
+      ++ (map (keymaps: {mode = ["n" "v"];} // keymaps) [
+        {
+          action = ''"+Y'';
+          key = "<leader>Y";
+        }
 
-      {
-        action = ''"+y'';
-        key = "<leader>y";
-      }
+        {
+          action = ''"+y'';
+          key = "<leader>y";
+        }
 
-      {
-        action = ''"_D'';
-        key = "<leader>D";
-      }
+        {
+          action = ''"_D'';
+          key = "<leader>D";
+        }
 
-      {
-        action = ''"_d'';
-        key = "<leader>d";
-      }
+        {
+          action = ''"_d'';
+          key = "<leader>d";
+        }
 
-      {
-        action = ''"_dP'';
-        key = "<leader>P";
-      }
+        {
+          action = ''"_dP'';
+          key = "<leader>P";
+        }
 
-      {
-        action = ''"_dP'';
-        key = "<leader>p";
-      }
-    ]);
+        {
+          action = ''"_dP'';
+          key = "<leader>p";
+        }
+      ]));
 }
