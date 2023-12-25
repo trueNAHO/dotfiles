67ccbe944c459b550921283226bcb63e76565197
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-12-25 11:56:09 +0100

    feat(modules/programs/nixvim/plugins/telescope): add 'TODO' keymapping
    Reference:
    
    * https://github.com/GaetanLepage/dotfiles/blob/641292375e721ad6b836f05e6594e79fddf2776f/home/modules/tui/neovim/todo.nix

--- modules/programs/nixvim/plugins/telescope/default.nix
+++ modules/programs/nixvim/plugins/telescope/default.nix
@@ -1,30 +1,46 @@
-let
-  leader = {
-    git = "<leader>g";
-    telescope = "<leader>t";
-  };
-in {
-  programs.nixvim.plugins.telescope = {
-    enable = true;
-    defaults.layout_strategy = "vertical";
+{
+  programs.nixvim = let
+    leader = {
+      git = "<leader>g";
+      telescope = "<leader>t";
+    };
 
-    keymaps = {
-      "${leader.git}C" = "git_bcommits";
-      "${leader.git}b" = "git_branches";
-      "${leader.git}c" = "git_commits";
-      "${leader.git}s" = "git_status";
-      "${leader.telescope}C" = "command_history";
-      "${leader.telescope}G" = "current_buffer_fuzzy_find";
-      "${leader.telescope}b" = "buffers";
-      "${leader.telescope}c" = "commands";
-      "${leader.telescope}f" = "find_files";
-      "${leader.telescope}g" = "live_grep";
-      "${leader.telescope}h" = "help_tags";
-      "${leader.telescope}m" = "man_pages";
-      "${leader.telescope}o" = "oldfiles";
-      "${leader.telescope}q" = "quickfix";
-      "${leader.telescope}r" = "resume";
-      "${leader.telescope}s" = "search_history";
+    todo = "TODO";
+  in {
+    keymaps = [
+      {
+        action = "function() require('telescope.builtin').live_grep({default_text='${todo}', initial_mode='normal'}) end";
+        key = "${leader.telescope}t";
+        lua = true;
+        mode = "n";
+        options.silent = true;
+      }
+    ];
+
+    match.TODO = todo;
+
+    plugins.telescope = {
+      enable = true;
+      defaults.layout_strategy = "vertical";
+
+      keymaps = {
+        "${leader.git}C" = "git_bcommits";
+        "${leader.git}b" = "git_branches";
+        "${leader.git}c" = "git_commits";
+        "${leader.git}s" = "git_status";
+        "${leader.telescope}C" = "command_history";
+        "${leader.telescope}G" = "current_buffer_fuzzy_find";
+        "${leader.telescope}b" = "buffers";
+        "${leader.telescope}c" = "commands";
+        "${leader.telescope}f" = "find_files";
+        "${leader.telescope}g" = "live_grep";
+        "${leader.telescope}h" = "help_tags";
+        "${leader.telescope}m" = "man_pages";
+        "${leader.telescope}o" = "oldfiles";
+        "${leader.telescope}q" = "quickfix";
+        "${leader.telescope}r" = "resume";
+        "${leader.telescope}s" = "search_history";
+      };
     };
   };
 }
