55529137c05de85ff29aa43d2ca1bdef456fbc37
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-12-26 16:23:38 +0100

    feat(modules/programs/nixvim/keymaps): add insert-blank-lines mappings
    Add count-supported keymaps for inserting blank lines above and below
    the current cursor position with the respective '<leader>O' and
    '<leader>o' keys.
    
    Reference:
    
    * https://github.com/monkoose/neovim-setup/blob/0725b1ef821c58d02fba0fa3c688f0ebc69ecca8/lua/custom/unimpaired.lua#L1-L20

--- modules/programs/nixvim/keymaps/default.nix
+++ modules/programs/nixvim/keymaps/default.nix
@@ -1,93 +1,128 @@
 {
   programs.nixvim.keymaps =
     map (keymaps: {options.silent = true;} // keymaps)
-    ((map (keymaps: {mode = "n";} // keymaps) [
-        {
-          action = "<C-d>zz";
-          key = "<C-d>";
-        }
+    ((map (keymaps: {mode = "n";} // keymaps) ([
+          {
+            action = "<C-d>zz";
+            key = "<C-d>";
+          }
 
-        {
-          action = "<C-u>zz";
-          key = "<C-u>";
-        }
+          {
+            action = "<C-u>zz";
+            key = "<C-u>";
+          }
 
-        {
-          action = "<C-w>+";
-          key = "<M-C-j>";
-        }
+          {
+            action = "<C-w>+";
+            key = "<M-C-j>";
+          }
 
-        {
-          action = "<C-w>-";
-          key = "<M-C-k>";
-        }
+          {
+            action = "<C-w>-";
+            key = "<M-C-k>";
+          }
 
-        {
-          action = "<C-w><";
-          key = "<M-C-h>";
-        }
+          {
+            action = "<C-w><";
+            key = "<M-C-h>";
+          }
 
-        {
-          action = "<C-w>>";
-          key = "<M-C-l>";
-        }
+          {
+            action = "<C-w>>";
+            key = "<M-C-l>";
+          }
 
-        {
-          action = "<C-w>W";
-          key = "<leader>k";
-        }
+          {
+            action = "<C-w>W";
+            key = "<leader>k";
+          }
 
-        {
-          action = "<C-w>_<C-w>|";
-          key = "<leader>F";
-        }
+          {
+            action = "<C-w>_<C-w>|";
+            key = "<leader>F";
+          }
 
-        {
-          action = "<C-w>c";
-          key = "<leader>Q";
-        }
+          {
+            action = "<C-w>c";
+            key = "<leader>Q";
+          }
 
-        {
-          action = "<C-w>w";
-          key = "<leader>j";
-        }
+          {
+            action = "<C-w>w";
+            key = "<leader>j";
+          }
 
-        {
-          action = "Nzzzv";
-          key = "N";
-        }
+          {
+            action = "Nzzzv";
+            key = "N";
+          }
 
-        {
-          action = ''
-            function()
-              local spell = not vim.opt.spell:get()
-              vim.opt.spell = spell
-              print(spell)
+          {
+            action = ''
+              function()
+                local spell = not vim.opt.spell:get()
+                vim.opt.spell = spell
+                print(spell)
+              end
+            '';
+
+            key = "<leader>s";
+            lua = true;
+          }
+
+          {
+            action = ''
+              function()
+                local virtual_text = not vim.diagnostic.config().virtual_text
+                vim.diagnostic.config({ virtual_text = virtual_text })
+                print(virtual_text)
+              end
+            '';
+
+            key = "<leader>v";
+            lua = true;
+          }
+
+          {
+            action = "nzzzv";
+            key = "n";
+          }
+        ]
+        ++ (let
+          insert_blank_line = line: ''
+            local lines = {}
+
+            for _ = 1, vim.v.count1 do
+              table.insert(lines, "")
             end
+
+            local line = ${line}
+
+            vim.api.nvim_buf_set_lines(0, line, line, true, lines)
           '';
+        in [
+          {
+            action = ''
+              function()
+                ${insert_blank_line "vim.fn.line('.') - 1"}
+              end
+            '';
 
-          key = "<leader>s";
-          lua = true;
-        }
+            key = "<leader>O";
+            lua = true;
+          }
 
-        {
-          action = ''
-            function()
-              local virtual_text = not vim.diagnostic.config().virtual_text
-              vim.diagnostic.config({ virtual_text = virtual_text })
-              print(virtual_text)
-            end
-          '';
+          {
+            action = ''
+              function()
+                ${insert_blank_line "vim.fn.line('.')"}
+              end
+            '';
 
-          key = "<leader>v";
-          lua = true;
-        }
-
-        {
-          action = "nzzzv";
-          key = "n";
-        }
-      ])
+            key = "<leader>o";
+            lua = true;
+          }
+        ])))
       ++ (map (keymaps: {mode = ["n" "v"];} // keymaps) [
         {
           action = ''"+Y'';
