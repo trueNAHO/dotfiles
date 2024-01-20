4590d4420e4ea708812898b921751b2b2f5b9b53
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-01-20 19:08:12 +0100

    feat(modules/homeManager/programs/taskwarrior): adapt urgency scoring
    Reference: https://taskwarrior.org/docs/urgency

--- modules/homeManager/programs/taskwarrior/default.nix
+++ modules/homeManager/programs/taskwarrior/default.nix
@@ -14,6 +14,27 @@
       # TODO: https://github.com/danth/stylix/pull/223
       colorTheme = "no-color";
 
+      config.urgency = {
+        active.coefficient = 1;
+        age.coefficient = 0;
+        annotations.coefficient = 0;
+        blocked.coefficient = -2;
+        blocking.coefficient = 0;
+        due.coefficient = 6;
+        project.coefficient = 0;
+        scheduled.coefficient = 1;
+        tags.coefficient = 0;
+
+        uda.priority = {
+          H.coefficient = 6;
+          L.coefficient = 2;
+          M.coefficient = 4;
+        };
+
+        user.tag.next.coefficient = 8;
+        waiting.coefficient = -1;
+      };
+
       enable = true;
     };
   };
