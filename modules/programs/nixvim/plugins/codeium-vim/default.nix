43277283d55ff0c0eb7d16186e9b8a1016698e2f
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-02-20 16:15:48 +0100

    fix(modules/programs/nixvim/plugins/codeium-vim): directory unavailable
    Create the directory prior to symlinking to avoid the following error:
    
    > ln: failed to create symbolic link
    > '/home/naho/.local/share/.codeium/config.json': No such file or
    > directory
    
    Follows: c4ded960a25a3607f8d5f454bc48362523395658

--- modules/programs/nixvim/plugins/codeium-vim/default.nix
+++ modules/programs/nixvim/plugins/codeium-vim/default.nix
@@ -6,13 +6,18 @@
   # TODO: Replace the activation script with an upstream VIM plugin option for
   # setting the config path storing the API key, similar to the 'config_path'
   # option in https://github.com/Exafunction/codeium.nvim.
-  home.activation.codeiumVim = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ln \
-      --force \
-      --symbolic \
-      ${config.age.secrets.modulesProgramsNixvimPluginsCodeium.path} \
-      "${config.xdg.dataHome}/.codeium/config.json"
-  '';
+  home.activation.codeiumVim = let
+    directory = "${config.xdg.dataHome}/.codeium";
+  in
+    lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir --parents "${directory}"
+
+      ln \
+        --force \
+        --symbolic \
+        ${config.age.secrets.modulesProgramsNixvimPluginsCodeium.path} \
+        "${directory}/config.json"
+    '';
 
   programs.nixvim.plugins.codeium-vim.enable = true;
 }
