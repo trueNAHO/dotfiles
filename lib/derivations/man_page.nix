49e51c31b243343c9e0325c1c81fe747abb55251
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-03-11 14:37:18 +0100

    refactor(lib/derivations/man_page): add man page derivation function
    Adding a man page derivation function allows separating the user and
    developer documentations without code duplication.

---
+++ lib/derivations/man_page.nix
@@ -0,0 +1,40 @@
+{
+  path,
+  pkgs,
+}:
+pkgs.stdenv.mkDerivation {
+  installPhase = let
+    man = "${placeholder "man"}/share/man";
+  in ''
+    tmp="$(mktemp --directory)"
+    trap "rm --force --recursive $tmp" EXIT
+
+    mkdir --parent "$out" "${man}"
+
+    ${pkgs.fd.pname} \
+      --extension adoc \
+      . \
+      ${path} \
+      -X \
+      ${pkgs.asciidoctor-with-extensions.meta.mainProgram} \
+      --attribute attribute-missing=warn \
+      --backend manpage \
+      --destination-dir "$tmp" \
+      --failure-level INFO
+
+    ${pkgs.fd.pname} --type file . "$tmp" |
+      while read -r file; do
+        filename="$(basename --suffix .gz "$file")"
+        output_directory="${man}/man''${filename##*.}"
+
+        mkdir --parent "$output_directory"
+        mv "$file" "$output_directory"
+      done
+
+  '';
+
+  name = "dotfiles";
+  nativeBuildInputs = with pkgs; [asciidoctor-with-extensions fd];
+  outputs = ["out" "man"];
+  src = ../..;
+}
--- modules/home/packages/dotfiles/default.nix
+++ modules/home/packages/dotfiles/default.nix
@@ -11,43 +11,10 @@
     modules.homeManager.programs.man.enable = true;
 
     home.packages = [
-      (pkgs.stdenv.mkDerivation
-        {
-          installPhase = let
-            man = "${placeholder "man"}/share/man";
-          in ''
-            tmp="$(mktemp --directory)"
-            trap "rm --force --recursive $tmp" EXIT
-
-            mkdir --parent "$out" "${man}"
-
-            ${pkgs.fd.pname} \
-              --extension adoc \
-              . \
-              docs \
-              -X \
-              ${pkgs.asciidoctor-with-extensions.meta.mainProgram} \
-              --attribute attribute-missing=warn \
-              --backend manpage \
-              --destination-dir "$tmp" \
-              --failure-level INFO
-
-            ${pkgs.fd.pname} --type file . "$tmp" |
-              while read -r file; do
-                filename="$(basename --suffix .gz "$file")"
-                output_directory="${man}/man''${filename##*.}"
-
-                mkdir --parent "$output_directory"
-                mv "$file" "$output_directory"
-              done
-
-          '';
-
-          name = "dotfiles";
-          nativeBuildInputs = with pkgs; [asciidoctor-with-extensions fd];
-          outputs = ["out" "man"];
-          src = ../../../..;
-        })
+      (import ../../../../lib/derivations/man_page.nix {
+        inherit pkgs;
+        path = "docs";
+      })
     ];
   };
 }
