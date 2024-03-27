73d7a5a36a80a4a05a55fc809b13ccee5e5cbf93
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-03-27 17:13:55 +0100

    feat(modules/homeManager/nixpkgs/config/allowUnfree): log at activation
    Log during the evaluation and activation phase.

--- modules/homeManager/nixpkgs/config/allowUnfree/default.nix
+++ modules/homeManager/nixpkgs/config/allowUnfree/default.nix
@@ -8,10 +8,15 @@
 
   config = let
     cfg = config.modules.homeManager.nixpkgs.config.allowUnfree;
+    string = "nixpkgs.config.allowUnfree = ${toString cfg.enable};";
   in {
-    nixpkgs.config.allowUnfree =
-      lib.info
-      "nixpkgs.config.allowUnfree = ${toString cfg.enable};"
-      cfg.enable;
+    home.activation.allowUnfree =
+      import
+      ../../../../../lib/modules/lib_hm_dag_entry_after_write_boundary_printf {
+        inherit string lib;
+        src = "modules.homeManager.nixpkgs.config.allowUnfree";
+      };
+
+    nixpkgs.config.allowUnfree = lib.info string cfg.enable;
   };
 }
