d7df3471a8ee5ec6c43466940442236a9b5f30d5
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-08 11:27:57 +0200

    docs(modules/agenix/homeManagerModules/default): add debugging message

--- modules/agenix/homeManagerModules/default/default.nix
+++ modules/agenix/homeManagerModules/default/default.nix
@@ -11,5 +11,21 @@
 
   config = lib.mkIf config.modules.agenix.homeManagerModules.default.enable {
     age.identityPaths = ["${config.home.homeDirectory}/.ssh/id_ed25519_age"];
+
+    home.activation = let
+      src = "modules.agenix.homeManagerModules.default";
+    in {
+      ${src} =
+        import
+        ../../../../lib/modules/lib_hm_dag_entry_before_write_boundary_printf {
+          inherit lib src;
+
+          string = let
+            keys = builtins.concatStringsSep ", " (
+              map toString config.age.identityPaths
+            );
+          in "at least one recipient key required: ${keys}";
+        };
+    };
   };
 }
