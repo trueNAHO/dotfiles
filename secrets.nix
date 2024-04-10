85ea079ca282c80eb4faacf427d278f6c4c24adb
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-10 18:21:14 +0200

    refactor(secrets): name public keys

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,7 @@
 let
-  publicKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrrgYSUQdMPznQBTYSr4jf1p9feRpVWjFuW1MdmtQM4"
-  ];
+  publicKeys = builtins.attrValues {
+    NAHO = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrrgYSUQdMPznQBTYSr4jf1p9feRpVWjFuW1MdmtQM4";
+  };
 in {
   "modules/homeManager/programs/borgmatic/encryption_passcommand.age".publicKeys = publicKeys;
   "modules/homeManager/programs/gh/gh_token.age".publicKeys = publicKeys;
