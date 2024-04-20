924bfdada6aeb7cc28a2c06899a9ae06118b0a61
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-20 16:10:20 +0200

    feat(modules/homeManager/programs/fish): change replace cursor

--- modules/homeManager/programs/fish/default.nix
+++ modules/homeManager/programs/fish/default.nix
@@ -260,6 +260,7 @@
               end
 
               set --global fish_cursor_insert line
+              set --global fish_cursor_replace underscore
               set --global fish_cursor_replace_one underscore
             end
 
