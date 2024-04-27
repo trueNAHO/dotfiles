9217393a6bd0314562e6454a3ca415040af95eed
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-27 13:33:28 +0200

    style(modules/programs/nixvim/plugins/leap): indent using two spaces

--- modules/programs/nixvim/plugins/leap/default.nix
+++ modules/programs/nixvim/plugins/leap/default.nix
@@ -12,11 +12,11 @@
         {
           action = ''
             function()
-                require("leap").leap({
-                    target_windows = vim.tbl_filter(function(win)
-                        return vim.api.nvim_win_get_config(win).focusable
-                    end, vim.api.nvim_tabpage_list_wins(0)),
-                })
+              require("leap").leap({
+                target_windows = vim.tbl_filter(function(win)
+                  return vim.api.nvim_win_get_config(win).focusable
+                end, vim.api.nvim_tabpage_list_wins(0)),
+              })
             end
           '';
           key = "<leader>f";
