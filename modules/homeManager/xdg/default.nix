aec7ef54a958b5f2747a6e910a6e47cb33e1e5f2
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-27 13:12:55 +0200

    refactor(modules/homeManager/xdg): extract common directory

--- modules/homeManager/xdg/default.nix
+++ modules/homeManager/xdg/default.nix
@@ -25,16 +25,19 @@
       enable = cfg.enable;
       mimeApps.enable = cfg.mimeApps.enable;
 
-      userDirs = lib.mkIf cfg.userDirs.enable {
-        desktop = config.home.sessionVariables.TMPDIR;
-        documents = config.home.sessionVariables.TMPDIR;
-        download = config.home.sessionVariables.TMPDIR;
-        music = "${config.home.homeDirectory}/p/music";
-        pictures = "${config.home.homeDirectory}/p/images";
-        publicShare = config.home.sessionVariables.TMPDIR;
-        templates = config.home.sessionVariables.TMPDIR;
-        videos = "${config.home.homeDirectory}/p/videos";
-      };
+      userDirs = let
+        personal = "${config.home.homeDirectory}/p";
+      in
+        lib.mkIf cfg.userDirs.enable {
+          desktop = config.home.sessionVariables.TMPDIR;
+          documents = config.home.sessionVariables.TMPDIR;
+          download = config.home.sessionVariables.TMPDIR;
+          music = "${personal}/music";
+          pictures = "${personal}/images";
+          publicShare = config.home.sessionVariables.TMPDIR;
+          templates = config.home.sessionVariables.TMPDIR;
+          videos = "${personal}/videos";
+        };
     };
   };
 }
