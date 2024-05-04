25e522966b666d73f9a37754d631d98c0f8c75cf
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-05-04 16:16:53 +0200

    refactor(modules/stylix): unconditionally set 'stylix.image' option
    Unconditionally set the 'stylix.image' option.
    
    Ideally, the currently unconditional 'stylix.image' option should be
    merged with the conditional 'lib.mkIf config.modules.stylix.enable'
    attribute set. [1]
    
    [1]: https://github.com/danth/stylix/issues/216

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -10,50 +10,61 @@ in {
   imports = [inputs.stylix.homeManagerModules.stylix];
   options.modules.stylix.enable = lib.mkEnableOption module;
 
-  config = lib.mkIf config.modules.stylix.enable {
-    home.activation.${module} =
-      lib.dotfiles.lib.hm.dag.entryBefore.writeBoundary.systemRequirement
-      module
-      "programs.dconf.enable = true;"
-      "";
-
-    stylix = {
-      cursor = {
-        name = "Bibata-Modern-Amber";
-        package = pkgs.bibata-cursors;
-        size = 22;
-      };
-
-      fonts = {
-        emoji = {
-          package = pkgs.noto-fonts-emoji;
-          name = "Noto Color Emoji";
-        };
-
-        monospace = {
-          package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
-          name = "FiraCodeNerdFont";
-        };
-
-        sansSerif = {
-          package = pkgs.ibm-plex;
-          name = "IBMPlexSans";
-        };
-
-        serif = {
-          package = pkgs.crimson;
-          name = "Crimson";
-        };
-
-        sizes.terminal = 7;
-      };
-
-      image = pkgs.fetchurl {
+  config = lib.mkMerge [
+    # TODO: Patch an upstream 'stylix.enable' option [1] to merge the
+    # unconditional 'stylix.image' option with the conditional 'lib.mkIf
+    # config.modules.stylix.enable' attribute set.
+    #
+    # [1]: https://github.com/danth/stylix/issues/216
+    {
+      stylix.image = pkgs.fetchurl {
         url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
         sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
       };
+    }
 
-      polarity = "dark";
-    };
-  };
+    (
+      lib.mkIf config.modules.stylix.enable {
+        home.activation.${module} =
+          lib.dotfiles.lib.hm.dag.entryBefore.writeBoundary.systemRequirement
+          module
+          "programs.dconf.enable = true;"
+          "";
+
+        stylix = {
+          cursor = {
+            name = "Bibata-Modern-Amber";
+            package = pkgs.bibata-cursors;
+            size = 22;
+          };
+
+          fonts = {
+            emoji = {
+              package = pkgs.noto-fonts-emoji;
+              name = "Noto Color Emoji";
+            };
+
+            monospace = {
+              package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+              name = "FiraCodeNerdFont";
+            };
+
+            sansSerif = {
+              package = pkgs.ibm-plex;
+              name = "IBMPlexSans";
+            };
+
+            serif = {
+              package = pkgs.crimson;
+              name = "Crimson";
+            };
+
+            sizes.terminal = 7;
+          };
+
+          polarity = "dark";
+        };
+      }
+    )
+  ];
 }
